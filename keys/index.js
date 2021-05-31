8bb1f1456f35387177beb29130786dfe60f2d57d
Udjin Stolboviy <JenyJenyy@gmail.com>
2021-05-31 14:45:13 +0300

    added project

---
+++ keys/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  MONGODB_URI:
+    "mongodb+srv://nodejs-shop:3yDs8HRuERKZLRr@cluster0.dgd0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  SESSION_SECRET: "some secret value",
+  SENDGRID_API_KEY:
+    "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",
+  EMAIL_FROM: "nodejs@email.ru",
+  BASE_URL: "http://localhost:3000",
+};
